5e2676115fc6568189a1b8cbb77575403d718145
XLannn <alanbijialex@gmail.com>
2025-09-18 23:40:06 +0530

    Add your descriptive message here

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -72,7 +72,7 @@ const Footer = () => {
         </div>
         <div className="border-t mt-8 pt-8 text-center text-sm text-muted-foreground">
           {/* Updated the copyright year */}
-          <p>&copy; 2025 PluggedIn. All rights reserved.</p>
+          <p>&copy; 2025 PLGDIN INNOVATONS LLP. All rights reserved.</p>
         </div>
       </div>
     </footer>
